bcb612db32d892a334ba354c47370249a40d57b9
jmulder <jmulder@overstock.com>
2018-12-08 11:16:53 -0700

    initial commit

---
+++ fighter/fighter.js
@@ -0,0 +1,52 @@
+const enemy = {
+  attack: 11,
+  criticalChance: 15,
+  defense: 3,
+  health: 100,
+  maxHealth: 100,
+  moves: ['retaliate', 'critical'],
+  name: 'enemy'
+}
+
+const player = {
+  attack: 10,
+  defense: 3,
+  health: 100,
+  maxHealth: 100,
+  name: 'player'
+}
+
+$('#attackButton1').click(() => attack('player'))
+
+function attack(atkr) {
+  const attacker = atkr === 'player' ? player : enemy
+  const target = atkr === 'player' ? enemy : player
+  const bar = atkr === 'player' ? $('#enemyHealthBar') : $('#playerHealthBar')
+
+  const damage = attacker.attack - target.defense
+  return $(bar).is(':animated') && target.health
+    ? ''
+    : reduceHealthBar(damage, target, bar)
+}
+
+function reduceHealthBar(damage, target, bar) {
+  target.health = Math.max(0, target.health - damage)
+  const mh = target.maxHealth
+  const h = target.health
+  const targetIsEnemy = target.name === 'enemy'
+  $(bar).animate({ width: (h / mh) * 100 + '%' }, 300)
+  setTimeout(
+    () => (h <= 0 ? showAlert(target) : targetIsEnemy ? attack('enemy') : null),
+    300
+  )
+}
+
+$('#attackButton2').click(() => reduceDefense('enemy'))
+
+function reduceDefense() {
+  enemy.defense = Math.max(0, enemy.defense - 3)
+}
+
+function showAlert(target) {
+  alert(target.name === 'enemy' ? 'VICTORY' : 'DEFEAT')
+}
